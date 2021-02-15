8ac55530085b36c2beefed47acc18c4e0c83286e
Jan Hazincak <jan.hazincak@gmail.com>
2021-02-15 02:25:56 +0000

    Navbar layout modified

--- includes/navigation.php
+++ includes/navigation.php
@@ -12,7 +12,7 @@
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
+                <ul class="nav navbar-nav mr-auto">
                     <?php 
                         $query = "SELECT * FROM categories";
                         $select_all_categories_query = mysqli_query($connection, $query);
@@ -23,46 +23,44 @@
                             echo "<li><a href='category.php?category=$cat_id&category_name=$cat_title'>{$cat_title}</a></li>";
 
                         };
+
                     ?>
-                    <li><a href="registration.php">Registration</a></li>
-<?php
-if(isset($_SESSION['username'])){
-    echo " <li class='dropdown'>
-    <a href='#' class='dropdown-toggle' data-toggle='dropdown'><i class='fa fa-user'></i> '{$_SESSION['username']}' <b class='caret'></b></a>
-    <ul class='dropdown-menu'>
-        <li>
-            <a href='userOptions.php?source=profile'><i class='fa fa-fw fa-user'></i> Profile</a>
-        </li>
-        <li>
-            <a href='userOptions.php?source=all_posts'><i class='fa fa-fw fa-user'></i> My posts</a>
-        </li>
-        <li>
-            <a href='userOptions.php?source=create_post'><i class='fa fa-fw fa-user'></i> Add post</a>
-        </li>
-        <li class='divider'></li>
-        <li>
-            <a href='includes/logout.php'><i class='fa fa-fw fa-power-off'></i> Log Out</a>
-        </l";
-}
-
-?>
-                
-                    </ul>
-                </li>
-               
-
-                </li>
-<?php
-if(isset($_SESSION['user_role'])){
-    if(isset($_GET['p_id'])){
-       $the_post_id = $_GET['p_id'];
-        echo "<li><a href='admin/posts.php?source=edit_post&p_id={$the_post_id}'>Edit Post</a></li>";
-    }
-
-}
-?>
-                    
                 </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <?php
+                        if(!isset($_SESSION['username'])){
+                            if($_SESSION['user_role'] =='admin'){
+                                echo "<li><a href='registration.php'>Registration</a></li>";
+                            }
+                        };
+                        if(isset($_SESSION['username'])){
+                            echo " <li class='dropdown'>
+                            <a href='#' class='dropdown-toggle' data-toggle='dropdown'><i class='fa fa-user'></i>Logged in as: {$_SESSION['username']} <b class='caret'></b></a>
+                            <ul class='dropdown-menu'>
+                                <li>
+                                    <a href='userOptions.php?source=profile'><i class='fa fa-fw fa-user'></i> Profile</a>
+                                </li>
+                                <li>
+                                    <a href='userOptions.php?source=all_posts'><i class='fa fa-fw fa-user'></i> My posts</a>
+                                </li>
+                                <li>
+                                    <a href='userOptions.php?source=create_post'><i class='fa fa-fw fa-user'></i> Add post</a>
+                                </li>
+                                <li class='divider'></li>
+                                <li>
+                                    <a href='includes/logout.php'><i class='fa fa-fw fa-power-off'></i> Log Out</a>
+                                </li>;
+                            </ul>";
+                        };
+
+                        if(isset($_SESSION['username'])){
+                            if($_SESSION['user_role'] =='admin'){
+                                echo "<li><a href='admin/index.php'>Admin</a></li>";
+                            }
+                        };
+                    ?>        
+                </ul>
+                            
 
             </div>
           
