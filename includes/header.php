27f991027636ee16cae526c4961850d73e65761c
Jan Hazincak <jan.hazincak@gmail.com>
2021-02-16 02:58:41 +0000

    upgrade public part to bootstrap4. Admin part remains bootstrap 3

--- includes/header.php
+++ includes/header.php
@@ -1,35 +1,26 @@
 
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+    <head>
+    <?php ob_start(); ?>
+    <?php session_start(); ?>
+    <?php include "functions.php"; ?>
 
-<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-<?php ob_start(); ?>
-<?php session_start(); ?>
-<?php include "functions.php"; ?>
+        <title>Food recipes website</title>
+    
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/styles.css" rel="stylesheet">
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Blog Home - Start Bootstrap Template</title>
-
-   
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <link href="css/blog-home.css" rel="stylesheet">
-    <link href="css/styles.css" rel="stylesheet">
-
-
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script src="https://cdn.ckeditor.com/ckeditor5/19.0.0/classic/ckeditor.js"></script>
-     <!-- jQuery -->
-     <script src="js/jquery.js"></script>
-
-
-</head>
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+        <script src="https://cdn.ckeditor.com/ckeditor5/19.0.0/classic/ckeditor.js"></script>
+         <!-- jQuery -->
+         <script src="js/jquery.js"></script>
+    </head>
 
 <body>
--- includes/navigation.php
+++ includes/navigation.php
@@ -1,17 +1,11 @@
-<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
             <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
                 <a class="navbar-brand" href="index.php">Home page</a>
             </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="nav navbar-nav mr-auto">
                     <?php 
                         $query = "SELECT * FROM categories";
@@ -20,40 +14,32 @@
                             $cat_title = $row["cat_title"];
                             $cat_id = $row["cat_id"];
 
-                            echo "<li><a href='category.php?category=$cat_id&category_name=$cat_title'>{$cat_title}</a></li>";
+                            echo "<li class='nav-item'><a class='nav-link' href='category.php?category=$cat_id&category_name=$cat_title'>{$cat_title}</a></li>";
 
                         };
 
                     ?>
                 </ul>
-                <ul class="nav navbar-nav navbar-right">
+                <ul class="navbar-nav ml-auto">
                     <?php
                         if(!isset($_SESSION['logged_in'])){
                             echo "<li><a href='registration.php'>Registration</a></li>";
                         };
                         if(isset($_SESSION['username'])){
-                            echo " <li class='dropdown'>
-                            <a href='#' class='dropdown-toggle' data-toggle='dropdown'><i class='fa fa-user'></i>Logged in as: {$_SESSION['username']} <b class='caret'></b></a>
-                            <ul class='dropdown-menu'>
-                                <li>
-                                    <a href='userOptions.php?source=profile'><i class='fa fa-fw fa-user'></i> Profile</a>
-                                </li>
-                                <li>
-                                    <a href='userOptions.php?source=all_posts'><i class='fa fa-fw fa-user'></i> My posts</a>
-                                </li>
-                                <li>
-                                    <a href='userOptions.php?source=create_post'><i class='fa fa-fw fa-user'></i> Add post</a>
-                                </li>
-                                <li class='divider'></li>
-                                <li>
-                                    <a href='includes/logout.php'><i class='fa fa-fw fa-power-off'></i> Log Out</a>
-                                </li>;
-                            </ul>";
+                            echo " <li class='nav-item dropdown'>
+                            <a class='nav-link dropdown-toggle' href='#' id='navbarDropdown' role='button' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'><i class='fa fa-user'></i>Logged in as: {$_SESSION['username']} <b class='caret'></b></a>
+                            <div class='dropdown-menu' aria-labelledby='navbarDropdown'>
+                                <a class='dropdown-item' href='userOptions.php?source=profile'><i class='fa fa-fw fa-user'></i> Profile</a>
+                                <a class='dropdown-item' href='userOptions.php?source=all_posts'><i class='fa fa-fw fa-user'></i> My posts</a>
+                                <a class='dropdown-item' href='userOptions.php?source=create_post'><i class='fa fa-fw fa-user'></i> Add post</a>
+                                <div class='dropdown-divider'></div>       
+                                <a class='dropdown-item' href='includes/logout.php'><i class='fa fa-fw fa-power-off'></i> Log Out</a>
+                            </div>";
                         };
 
                         if(isset($_SESSION['username'])){
                             if($_SESSION['user_role'] =='admin'){
-                                echo "<li><a href='admin/index.php'>Admin</a></li>";
+                                echo "<li class='nav-item'><a class='nav-link' href='admin/index.php'>Admin</a></li>";
                             }
                         };
                     ?>        
@@ -63,6 +49,4 @@
             </div>
           
             <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
     </nav>
